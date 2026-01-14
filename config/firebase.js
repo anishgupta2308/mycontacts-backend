ea20fa6899acc5c44c332abd0ab72f40bf25efa0
Anish <anishrajgupta616@gmail.com>
2026-01-14 12:18:45 +0530

    Finalize backend for Cloud Run

--- config/firebase.js
+++ config/firebase.js
@@ -1,11 +1,7 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+
+admin.initializeApp();
 
 const db = admin.firestore();
 module.exports = db;
-
-
